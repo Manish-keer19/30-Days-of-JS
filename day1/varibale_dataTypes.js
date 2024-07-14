d1b5d5da3db745d4ce9f28db4bd2ee0d80444c1b
manish <manishkeer530@gmail.com>
2024-07-14 19:04:43 +0530

    1st day completed

---
+++ day1/varibale_dataTypes.js
@@ -0,0 +1,43 @@
+var num = 20;
+console.log(20);
+
+let name = "manish";
+console.log(name);
+
+const flag = true;
+console.log(flag);
+
+let name1 = "vishal";
+console.log(typeof name1);
+let age = 20;
+console.log(typeof age);
+
+let bool = true;
+console.log(bool);
+
+let obj = {
+  name: "manish",
+  age: 20,
+};
+
+console.log(typeof obj);
+
+let arr = [1, 2, 3, 4, 5, 6];
+console.log(typeof arr);
+
+let arr1 = new Array(1, 2, 3, 4, 5, 6);
+console.log(typeof arr1);
+
+let marks = 90;
+console.log(marks);
+marks = 99;
+console.log(marks);
+
+const val = 20;
+console.log(val);
+
+val = 30;
+// it give error because we can't reasign the const value
+// console.log(val);
+
+
